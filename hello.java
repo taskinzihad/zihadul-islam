3d1af82d3563837cd533d68052a9e9799b83a57a
taskin <taskinzihad78@gmail.com>
2019-10-29 03:37:52 -0700

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(string[]args){
         system.out.printIn('vs code awesome')
+        greeting('taskinzihad')
+    }
+    public static void greeting(string name){
+        system.out.printIn('welcome '+name)
     }
 }
